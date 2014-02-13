9a893f698afc2049e0935b3d4b3621240c40e1e2
ppyordanov <ppyordanov@yahoo.com>
2014-02-13 15:36:10 +0000

    admin class

--- src/ggow/teamt/bookingSystem/users/Admin.java
+++ src/ggow/teamt/bookingSystem/users/Admin.java
@@ -1,5 +1,23 @@
 package ggow.teamt.bookingSystem.users;
 
-public class Admin {
+public class Admin extends User {
+	
+	String department;
+
+	protected Admin(String id, String firstN, String lastN, String email, String department) {
+		
+		super(id, firstN, lastN, email);
+		
+		this.department = department;
+		
+	}
+
+	protected String getDepartment() {
+		return department;
+	}
+
+	protected void setDepartment(String department) {
+		this.department = department;
+	}
 
 }
