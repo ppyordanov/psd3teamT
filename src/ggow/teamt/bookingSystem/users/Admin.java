44eabf569f63143962b13aaa1014a78955ea0e54
ppyordanov <ppyordanov@yahoo.com>
2014-02-13 16:19:00 +0000

    str. impr. + tostring

--- src/ggow/teamt/bookingSystem/users/Admin.java
+++ src/ggow/teamt/bookingSystem/users/Admin.java
@@ -1,10 +1,17 @@
 package ggow.teamt.bookingSystem.users;
 
+import ggow.teamt.bookingSystem.course.Course;
+import ggow.teamt.bookingSystem.course.Location;
+import ggow.teamt.bookingSystem.course.Session;
+
+import java.util.LinkedList;
+
 public class Admin extends User {
 	
 	String department;
+	protected LinkedList<Course> courseList;
 
-	protected Admin(String id, String firstN, String lastN, String email, String department) {
+	public Admin(String id, String firstN, String lastN, String email, String department) {
 		
 		super(id, firstN, lastN, email);
 		
@@ -12,12 +19,38 @@ public class Admin extends User {
 		
 	}
 
-	protected String getDepartment() {
+	public String getDepartment() {
 		return department;
 	}
 
-	protected void setDepartment(String department) {
+	public void setDepartment(String department) {
 		this.department = department;
 	}
-
+	
+	public void createCourse(String name, int classSize, LinkedList<Session> sessions, Lecturer lecturer){
+		
+		Course newC = new Course(name,classSize,sessions, lecturer);
+		courseList.add(newC);
+		
+	}
+	
+	public void removeCourse(Course remC){
+		courseList.remove(remC);
+	}
+	
+	public void createSession(Course C, Session s){
+		
+		C.getSessions().add(s);
+		
+	}
+	
+	public void changeLocation(Session S, Location l){
+		
+		S.setLocation(l);
+	}
+	
+	public String toString(){
+		return "User id: " + this.id + "\nName: " + this.firstN + " " + this.lastN + "\nEmail: " + this.email + "\nUser type: admin" + "\nDepartment: " + this.department; 
+	}
+	
 }
