ca8035242ddadfb94bf0b451c51420e679db8cf6
Georgi Dimitrov <gogodi91@gmail.com>
2014-02-26 14:51:56 +0000

    added attributes to Session

--- src/uk/ac/glasgow/bookingSystem/components/course/Session.java
+++ src/uk/ac/glasgow/bookingSystem/components/course/Session.java
@@ -1,6 +1,17 @@
 package uk.ac.glasgow.bookingSystem.components.course;
 
 import java.sql.Time;
+import java.sql.Date;
+/*
+Need to add following attributes:
+
+    array of all sessions
+
+Also error checking when setting these.
+
+A method to getAllDates() and fill out the array for the session.
+*/
+
 
 public class Session {
 
@@ -10,6 +21,11 @@ public class Session {
 	public Time startTime;
 	public Time endTime;
 	public int duration; //in minutes
+	public String frequency;//once, weekly, bi-monthly, monthly, yearly
+	public Date startDate;
+	public Date endDate;
+	public Boolean required;//is this course required? (true for Yes or false for No)
+	
 	
 	public Session() {
 		name = null;
@@ -18,10 +34,14 @@ public class Session {
 		startTime = null;
 		endTime = null;
 		duration = 0;
+		frequency = null;
+		startDate = null;
+		endDate = null;
+		required = null;
 	}
 	
 	public Session(String name, Course course, Location location,
-			Time startTime, Time endTime, int duration) {
+			Time startTime, Time endTime, int duration, String frequency, Date startDate, Date endDate, Boolean required) {
 		super();
 		this.name = name;
 		this.course = course;
@@ -29,6 +49,10 @@ public class Session {
 		this.startTime = startTime;
 		this.endTime = endTime;
 		this.duration = duration;
+		this.frequency = frequency;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.required = required;		
 	}
 
 	public String getName() {
@@ -77,6 +101,21 @@ public class Session {
 
 	public void setDuration(int duration) {
 		this.duration = duration;
+	}
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
 	}
 	
 	
