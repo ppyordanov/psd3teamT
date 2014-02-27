82062ccff30d51565526e5ae545c7fc467908b09
U-PeterYordanov\Peter Yordanov <ppyordanov@yahoo.com>
2014-02-27 16:35:04 +0000

    check courses sugned up // functions getSessionsSignedUp() setSessionsSignedUp() checkSessionsSignedUp()

--- src/uk/ac/glasgow/bookingSystem/components/users/Student.java
+++ src/uk/ac/glasgow/bookingSystem/components/users/Student.java
@@ -4,19 +4,21 @@ package uk.ac.glasgow.bookingSystem.components.users;
 import java.util.LinkedList;
 
 import uk.ac.glasgow.bookingSystem.components.course.Course;
+import uk.ac.glasgow.bookingSystem.components.course.Session;
 
 public class Student extends User{
 
 	protected boolean isTutor;
 	protected LinkedList<Course> courseList;
-	
+	protected LinkedList<Session> sessionsSignedUp;
 
 	public Student() {
 		super();
 		isTutor = false;
 		courseList = null;
+		sessionsSignedUp=0;
 	}
-	
+
 
 	public Student(String id, String firstN, String lastN, String email, boolean isTutor, LinkedList<Course> courseList) {
 		
@@ -41,6 +43,40 @@ public class Student extends User{
 
 	public void setCourseList(LinkedList<Course> courseList) {
 		this.courseList = courseList;
+	}
+	
+	public LinkedList<Session> getSessionsSignedUp() {
+		return sessionsSignedUp;
+	}
+
+
+	public void setSessionsSignedUp(LinkedList<Session> sessionsSignedUp) {
+		this.sessionsSignedUp = sessionsSignedUp;
+	}
+	
+	public String checkSessionsSignedUp(){
+		
+		String checkResult ="";
+		
+		for(Course c : courseList){
+			
+			for(Session s: c.getSessions()){
+				
+				if(s.getRequired()){
+					
+					if(!this.getSessionsSignedUp().contains(s)){
+						
+						checkResult += "Not signed up for session: " + s.getName() + "\n" + "Course: " + c.getName();
+						
+					}
+					
+				}
+
+			}
+		}
+		
+		return checkResult;
+		
 	}
 	
 	public String toString(){
